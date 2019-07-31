2de534f7808244182a4d84d373ff358684d996c8
Zach Morrissey <zachmor05@gmail.com>
2019-07-30 20:00:58 -0700

    formatting and add .vscode to gitignore

--- bucket.lua
+++ bucket.lua
@@ -115,7 +115,8 @@ end
 
 --- collision check
 function iscolliding(obj1, obj2)
-	return (obj1.x < (obj2.x + obj2.w) and obj2.x < (obj1.x + obj1.w) and obj1.y < (obj2.y + obj2.h) and
+	return (obj1.x < (obj2.x + obj2.w) and obj2.x < (obj1.x + obj1.w) and
+		obj1.y < (obj2.y + obj2.h) and
 		obj2.y < (obj1.y + obj1.h))
 end
 
